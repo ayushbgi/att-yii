1c6ac0bee25056558ded79c6355d829a7c6a65d9
ayushbgi <ayush@businessgateways.com>
2019-11-17 15:07:23 +0530

    ayush-2

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -41,7 +41,11 @@ AppAsset::register($this);
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
-            ['label' => 'Home', 'url' => ['/site/index']],
+          Yii::$app->user->isGuest ? (
+              ['label' => 'Home', 'url' => ['/site/index']]
+          ) : (['label' => 'Home', 'url' => ['/site/home']]
+          )
+            ,
             ['label' => 'About', 'url' => ['/site/about']],
             ['label' => 'Contact', 'url' => ['/site/contact']],
             Yii::$app->user->isGuest ? (
